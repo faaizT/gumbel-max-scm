6c6721d0c50829ce5066191782cc19c8f58c6fbe
faaizT <faaiztaufiq@gmail.com>
2021-04-07 00:03:11 +0100

    wrote a script to generate observational data

---
+++ generate_data_main.py
@@ -0,0 +1,80 @@
+from sepsisSimDiabetes.State import State
+from sepsisSimDiabetes.Action import Action
+from sepsisSimDiabetes.DataGenerator import DataGenerator
+import sepsisSimDiabetes.MDP as simulator
+import cf.counterfactual as cf
+import pickle
+import argparse
+import numpy as np
+from scipy.linalg import block_diag
+import mdptoolboxSrc.mdp as mdptools
+import pandas as pd
+
+
+def main(args):
+    DISCOUNT_Pol = 0.99  # Used for computing optimal policies
+    PROB_DIAB = 0.2
+    PHYS_EPSILON = 0.05  # Used for sampling using physician pol as eps greedy
+    n_actions = Action.NUM_ACTIONS_TOTAL
+    n_components = 2
+
+    with open(args.physpol, "rb") as f:
+        mdict = pickle.load(f)
+    tx_mat = mdict["tx_mat"]
+    r_mat = mdict["r_mat"]
+    p_mixture = np.array([1 - PROB_DIAB, PROB_DIAB])
+
+    tx_mat_full = np.zeros((n_actions, State.NUM_FULL_STATES, State.NUM_FULL_STATES))
+    r_mat_full = np.zeros((n_actions, State.NUM_FULL_STATES, State.NUM_FULL_STATES))
+
+    for a in range(n_actions):
+        tx_mat_full[a, ...] = block_diag(tx_mat[0, a, ...], tx_mat[1, a, ...])
+        r_mat_full[a, ...] = block_diag(r_mat[0, a, ...], r_mat[1, a, ...])
+    fullMDP = cf.MatrixMDP(tx_mat_full, r_mat_full)
+    fullPol = fullMDP.policyIteration(discount=DISCOUNT_Pol, eval_type=1)
+
+    physPolSoft = np.copy(fullPol)
+    physPolSoft[physPolSoft == 1] = 1 - PHYS_EPSILON
+    physPolSoft[physPolSoft == 0] = PHYS_EPSILON / (n_actions - 1)
+    dgen = DataGenerator()
+    (
+        states,
+        actions,
+        lengths,
+        rewards,
+        diab,
+        emp_tx_totals,
+        emp_r_totals,
+    ) = dgen.simulate(
+        args.nsims,
+        args.nsteps,
+        policy=physPolSoft,
+        policy_idx_type="full",
+        p_diabetes=PROB_DIAB,
+        output_state_idx_type="full",
+        use_tqdm=False,
+        state_as_vectors=True,
+    )  # True, tqdm_desc='Behaviour Policy Simulation')
+    # results = pd.DataFrame()
+    # for i in range(args.nsims)
+    print(states[0])
+    states_reshaped = np.reshape(states, (args.nsteps * args.nsims, 8))
+    print(states_reshaped[0:20])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--nsteps", help="maximum number of steps in trajectory", type=int, default=20
+    )
+    parser.add_argument(
+        "--nsims", help="number of trajectories", type=int, default=1000
+    )
+    parser.add_argument(
+        "--physpol",
+        help="file containing reward and transition matrix for physpol",
+        type=str,
+        default="/data/localhost/taufiq/gumbel-max-scm-exportdir/data/diab_txr_mats-replication.pkl",
+    )
+    args = parser.parse_args()
+    main(args)
--- sepsisSimDiabetes/State.py
+++ sepsisSimDiabetes/State.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-'''
+"""
 Includes blood glucose level proxy for diabetes: 0-3
     (lo2 - counts as abnormal, lo1, normal, hi1, hi2 - counts as abnormal)
 Initial distribution:
     [.05, .15, .6, .15, .05] for non-diabetics and [.01, .05, .15, .6, .19] for diabetics
-'''
+"""
+
 
 class State(object):
 
@@ -14,19 +15,21 @@ class State(object):
     NUM_PROJ_OBS_STATES = int(720 / 5)  # Marginalizing over glucose
     NUM_FULL_STATES = int(NUM_OBS_STATES * NUM_HID_STATES)
 
-    def __init__(self,
-            state_idx = None, idx_type = 'obs',
-            diabetic_idx = None, state_categs = None):
+    def __init__(
+        self, state_idx=None, idx_type="obs", diabetic_idx=None, state_categs=None
+    ):
 
         assert state_idx is not None or state_categs is not None
-        assert ((diabetic_idx is not None and diabetic_idx in [0, 1]) or
-                (state_idx is not None and idx_type == 'full'))
+        assert (diabetic_idx is not None and diabetic_idx in [0, 1]) or (
+            state_idx is not None and idx_type == "full"
+        )
 
-        assert idx_type in ['obs', 'full', 'proj_obs']
+        assert idx_type in ["obs", "full", "proj_obs"]
 
         if state_idx is not None:
             self.set_state_by_idx(
-                    state_idx, idx_type=idx_type, diabetic_idx=diabetic_idx)
+                state_idx, idx_type=idx_type, diabetic_idx=diabetic_idx
+            )
         elif state_categs is not None:
             assert len(state_categs) == 7, "must specify 7 state variables"
             self.hr_state = state_categs[0]
@@ -57,108 +60,113 @@ class State(object):
         full (1440)
         :param diabetic_idx: If full state index not given, this is required
         """
-        if idx_type == 'obs':
-            term_base = State.NUM_OBS_STATES/3 # Starts with heart rate
-        elif idx_type == 'proj_obs':
-            term_base = State.NUM_PROJ_OBS_STATES/3
-        elif idx_type == 'full':
-            term_base = State.NUM_FULL_STATES/2 # Starts with diab
+        if idx_type == "obs":
+            term_base = State.NUM_OBS_STATES / 3  # Starts with heart rate
+        elif idx_type == "proj_obs":
+            term_base = State.NUM_PROJ_OBS_STATES / 3
+        elif idx_type == "full":
+            term_base = State.NUM_FULL_STATES / 2  # Starts with diab
 
         # Start with the given state index
         mod_idx = state_idx
 
-        if idx_type == 'full':
-            self.diabetic_idx = np.floor(mod_idx/term_base).astype(int)
+        if idx_type == "full":
+            self.diabetic_idx = np.floor(mod_idx / term_base).astype(int)
             mod_idx %= term_base
-            term_base /= 3 # This is for heart rate, the next item
+            term_base /= 3  # This is for heart rate, the next item
         else:
             assert diabetic_idx is not None
             self.diabetic_idx = diabetic_idx
 
-        self.hr_state = np.floor(mod_idx/term_base).astype(int)
+        self.hr_state = np.floor(mod_idx / term_base).astype(int)
 
         mod_idx %= term_base
         term_base /= 3
-        self.sysbp_state = np.floor(mod_idx/term_base).astype(int)
+        self.sysbp_state = np.floor(mod_idx / term_base).astype(int)
 
         mod_idx %= term_base
         term_base /= 2
-        self.percoxyg_state = np.floor(mod_idx/term_base).astype(int)
+        self.percoxyg_state = np.floor(mod_idx / term_base).astype(int)
 
-        if idx_type == 'proj_obs':
+        if idx_type == "proj_obs":
             self.glucose_state = 2
         else:
             mod_idx %= term_base
             term_base /= 5
-            self.glucose_state = np.floor(mod_idx/term_base).astype(int)
+            self.glucose_state = np.floor(mod_idx / term_base).astype(int)
 
         mod_idx %= term_base
         term_base /= 2
-        self.antibiotic_state = np.floor(mod_idx/term_base).astype(int)
+        self.antibiotic_state = np.floor(mod_idx / term_base).astype(int)
 
         mod_idx %= term_base
         term_base /= 2
-        self.vaso_state = np.floor(mod_idx/term_base).astype(int)
+        self.vaso_state = np.floor(mod_idx / term_base).astype(int)
 
         mod_idx %= term_base
         term_base /= 2
-        self.vent_state = np.floor(mod_idx/term_base).astype(int)
+        self.vent_state = np.floor(mod_idx / term_base).astype(int)
 
-    def get_state_idx(self, idx_type='obs'):
-        '''
+    def get_state_idx(self, idx_type="obs"):
+        """
         returns integer index of state: significance order as in categorical array
-        '''
-        if idx_type == 'obs':
-            categ_num = np.array([3,3,2,5,2,2,2])
+        """
+        if idx_type == "obs":
+            categ_num = np.array([3, 3, 2, 5, 2, 2, 2])
             state_categs = [
-                    self.hr_state,
-                    self.sysbp_state,
-                    self.percoxyg_state,
-                    self.glucose_state,
-                    self.antibiotic_state,
-                    self.vaso_state,
-                    self.vent_state]
-        elif idx_type == 'proj_obs':
-            categ_num = np.array([3,3,2,2,2,2])
+                self.hr_state,
+                self.sysbp_state,
+                self.percoxyg_state,
+                self.glucose_state,
+                self.antibiotic_state,
+                self.vaso_state,
+                self.vent_state,
+            ]
+        elif idx_type == "proj_obs":
+            categ_num = np.array([3, 3, 2, 2, 2, 2])
             state_categs = [
-                    self.hr_state,
-                    self.sysbp_state,
-                    self.percoxyg_state,
-                    self.antibiotic_state,
-                    self.vaso_state,
-                    self.vent_state]
-        elif idx_type == 'full':
-            categ_num = np.array([2,3,3,2,5,2,2,2])
+                self.hr_state,
+                self.sysbp_state,
+                self.percoxyg_state,
+                self.antibiotic_state,
+                self.vaso_state,
+                self.vent_state,
+            ]
+        elif idx_type == "full":
+            categ_num = np.array([2, 3, 3, 2, 5, 2, 2, 2])
             state_categs = [
-                    self.diabetic_idx,
-                    self.hr_state,
-                    self.sysbp_state,
-                    self.percoxyg_state,
-                    self.glucose_state,
-                    self.antibiotic_state,
-                    self.vaso_state,
-                    self.vent_state]
+                self.diabetic_idx,
+                self.hr_state,
+                self.sysbp_state,
+                self.percoxyg_state,
+                self.glucose_state,
+                self.antibiotic_state,
+                self.vaso_state,
+                self.vent_state,
+            ]
 
         sum_idx = 0
         prev_base = 1
         for i in range(len(state_categs)):
             idx = len(state_categs) - 1 - i
-            sum_idx += prev_base*state_categs[idx]
+            sum_idx += prev_base * state_categs[idx]
             prev_base *= categ_num[idx]
         return sum_idx
 
     def __eq__(self, other):
-        '''
+        """
         override equals: two states equal if all internal states same
-        '''
-        return isinstance(other, self.__class__) and \
-            self.hr_state == other.hr_state and \
-            self.sysbp_state == other.sysbp_state and \
-            self.percoxyg_state == other.percoxyg_state and \
-            self.glucose_state == other.glucose_state and \
-            self.antibiotic_state == other.antibiotic_state and \
-            self.vaso_state == other.vaso_state and \
-            self.vent_state == other.vent_state
+        """
+        return (
+            isinstance(other, self.__class__)
+            and self.hr_state == other.hr_state
+            and self.sysbp_state == other.sysbp_state
+            and self.percoxyg_state == other.percoxyg_state
+            and self.glucose_state == other.glucose_state
+            and self.antibiotic_state == other.antibiotic_state
+            and self.vaso_state == other.vaso_state
+            and self.vent_state == other.vent_state
+        )
 
     def __ne__(self, other):
         return not self.__eq__(other)
@@ -167,9 +175,9 @@ class State(object):
         return self.get_state_idx()
 
     def get_num_abnormal(self):
-        '''
+        """
         returns number of abnormal conditions
-        '''
+        """
         num_abnormal = 0
         if self.hr_state != 1:
             num_abnormal += 1
@@ -182,48 +190,68 @@ class State(object):
         return num_abnormal
 
     def on_treatment(self):
-        '''
+        """
         returns True iff any of 3 treatments active
-        '''
-        if self.antibiotic_state == 0 and \
-            self.vaso_state == 0 and self.vent_state == 0:
+        """
+        if self.antibiotic_state == 0 and self.vaso_state == 0 and self.vent_state == 0:
             return False
         return True
 
     def on_antibiotics(self):
-        '''
+        """
         returns True iff antibiotics active
-        '''
+        """
         return self.antibiotic_state == 1
 
     def on_vasopressors(self):
-        '''
+        """
         returns True iff vasopressors active
-        '''
+        """
         return self.vaso_state == 1
 
     def on_ventilation(self):
-        '''
+        """
         returns True iff ventilation active
-        '''
+        """
         return self.vent_state == 1
 
     def copy_state(self):
-        return State(state_categs = [
-            self.hr_state,
-            self.sysbp_state,
-            self.percoxyg_state,
-            self.glucose_state,
-            self.antibiotic_state,
-            self.vaso_state,
-            self.vent_state],
-            diabetic_idx=self.diabetic_idx)
+        return State(
+            state_categs=[
+                self.hr_state,
+                self.sysbp_state,
+                self.percoxyg_state,
+                self.glucose_state,
+                self.antibiotic_state,
+                self.vaso_state,
+                self.vent_state,
+            ],
+            diabetic_idx=self.diabetic_idx,
+        )
 
     def get_state_vector(self):
-        return np.array([self.hr_state,
-            self.sysbp_state,
-            self.percoxyg_state,
-            self.glucose_state,
-            self.antibiotic_state,
-            self.vaso_state,
-            self.vent_state]).astype(int)
+        return np.array(
+            [
+                self.hr_state,
+                self.sysbp_state,
+                self.percoxyg_state,
+                self.glucose_state,
+                self.antibiotic_state,
+                self.vaso_state,
+                self.vent_state,
+            ]
+        ).astype(int)
+
+    def get_full_state_vector(self):
+        return np.array(
+            [
+                self.hr_state,
+                self.sysbp_state,
+                self.percoxyg_state,
+                self.glucose_state,
+                self.antibiotic_state,
+                self.vaso_state,
+                self.vent_state,
+                self.diabetic_idx,
+            ]
+        ).astype(int)
