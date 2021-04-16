dc003dfef2ebf90374467e681e3dbecf421863fc
faaizT <faaiztaufiq@gmail.com>
2021-04-16 23:22:16 +0100

    Corrected the emission function

--- sepsisSimDiabetes/utils/HelperFunctions.py
+++ sepsisSimDiabetes/utils/HelperFunctions.py
@@ -2,6 +2,5 @@ import numpy as np
 
 
 def noisy(var, max_val, min_val=0):
-    probs = np.ones(max_val + 1 - min_val) * 0.05
-    probs[var] = 1 - 0.05 * max_val
-    return np.random.choice(range(min_val, max_val + 1), p=probs)
+    vals = np.array([max(min_val, var-1), var, min(max_val, var+1)])
+    return np.random.choice(vals, p=np.array([0.05, 0.90, 0.05]))
